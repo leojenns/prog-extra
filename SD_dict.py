876221b8048a42c9991007791f5e0462eccdf005
leojenns <leo.jenns.s2@gmail.com>
2022-12-23 16:01:51 +0100

    update

--- SD_dict.py
+++ SD_dict.py
@@ -13,3 +13,19 @@ for naam in SD:
         total += grade  # total = total + grade
     print(f'cijfer van {naam} = {total/len(SD[naam])}')
 
+voldoende  = []
+onvoldoende = []
+
+for naam in SD:
+    total  = 0 
+    for grade in SD[naam]:
+        total = total + grade
+    gemiddelde = total / len(SD[naam])
+    if gemiddelde >= 5.5:
+        voldoende.append(naam)
+    else:
+        onvoldoende.append(naam)
+
+        
+print(f'{voldoende} hebben een voldoende')
+print(f'{onvoldoende} hebben een onvoldoende')
